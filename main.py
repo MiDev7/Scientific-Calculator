495d36e2985fe9c5bee4b06528240df0f2470610
MiDev7 <razafintsalamamikaia@gmail.com>
2023-07-11 19:01:21 +0400

    Theme

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import threading, time
 import math
 import customtkinter as ctk
 import sympy as sp
+from PIL import Image
 from sympy.printing.str import StrPrinter
+import tkinter as tk
+
+# this the function to be used to change the
+# ctk.set_appearance_mode("light")
 
 
 class App(ctk.CTk):
@@ -32,7 +37,8 @@ class App(ctk.CTk):
 
         self.title("Calculator")
 
-        # Multithreading
+        # !-----------------VARIABLE-------------
+        self.history = []
 
         #! --------------VARIABLE--------------------------
         self.formula = ctk.StringVar(value="")
@@ -47,24 +53,43 @@ class App(ctk.CTk):
         self.log_bool = False
         self.shift = False
         # !-------------WIDGETS------------------------------
-        Entry(self, self.result, self.formula)
+        self.entry = Entry(self, self.result, self.formula)
 
         self.frame_normal = NumberFrame(self, self.num_press)
         self.frame_scientific = ScientificFrame(self, self.num_press)
+        self.frame_programmer = Programmer(self)
         # testing
+        self.image = ctk.CTkImage(
+            light_image=Image.open("image/theme.png"),
+            dark_image=Image.open("image/dark.png"),
+            size=(30, 30),
+        )
+        self.theme_button = ctk.CTkButton(
+            self,
+            text="",
+            fg_color=PRIMARY,
+            hover_color=PRIMARY,
+            text_color=WHITE,
+            image=self.image,
+            height=30,
+            corner_radius=50,
+            width=30,
+            command=self.toggle_theme,
+        )
 
+        self.theme_button.place(relx=1, rely=0.0, anchor="ne")
         self.option = ctk.CTkOptionMenu(
             self,
-            values=["Normal", "Scientific"],
-            fg_color=ACCENTBROWN,
-            dropdown_fg_color=ACCENTBROWN,
+            values=["Normal", "Scientific", "Programmer"],
+            fg_color=GREY,
+            dropdown_fg_color=GREY,
             width=100,
-            button_color=DEEPBROWN,
-            button_hover_color=ACCENTBROWN,
-            text_color=PRIMARY,
+            button_color=GREY,
+            button_hover_color=GREY,
+            text_color=WHITE,
             corner_radius=5,
-            dropdown_text_color=PRIMARY,
-            dropdown_hover_color=DEEPBROWN,
+            dropdown_text_color=WHITE,
+            dropdown_hover_color=ACCENTGREY,
             command=self.scientific,
         )
 
@@ -74,19 +99,19 @@ class App(ctk.CTk):
             self,
             text="Differentiate",
             variable=self.differentiate,
-            fg_color=ACCENTBROWN,
-            text_color=ACCENTBROWN,
+            fg_color=GREY,
+            bg_color="transparent",
+            text_color=WHITE,
             checkbox_height=20,
             checkbox_width=20,
+            corner_radius=0,
             onvalue="on",
             offvalue="off",
-            checkmark_color=PRIMARY,
-            border_color=ACCENTBROWN,
-            hover_color=DEEPBROWN,
+            checkmark_color=WHITE,
+            border_color=GREY,
+            hover_color=ACCENTGREY,
             command=lambda: print(self.differentiate.get()),
         )
-
-        self.diff_widget.place(relx=0.5, rely=0.0, anchor="ne")
 
         # * -----------KEY BINDING TO EXIT THE APP -------------------------
         self.bind("<Escape>", lambda _: self.quit())
@@ -94,26 +119,42 @@ class App(ctk.CTk):
         self.scientific("Normal")
         self.mainloop()
 
+    def toggle_theme(self):
+        theme = ctk.get_appearance_mode()
+        print(theme)
+        if theme == "Light":
+            ctk.set_appearance_mode("dark")
+        else:
+            ctk.set_appearance_mode("light")
+
+    def calculate_result(self, *args):
+        result = "".join(self.display_num)
+        operation = "".join(self.operation)
+        self.formula.set(result)
+        print(operation)
+        if self.differentiate.get() == "off":
+            try:
+                result = eval(operation)
+                self.result.set(str(result))
+
+            except SyntaxError:
+                self.result.set("SYNTAX ERROR")
+            except ZeroDivisionError:
+                self.result.set("MATH ERROR")
+        else:
+            self.differentiation_operation(operation)
+
+        self.operation.clear()
+        self.display_num.clear()
+
     def num_press(self, value):
         if value == "=":
-            result = "".join(self.display_num)
-            operation = "".join(self.operation)
-            self.formula.set(result)
-            print(operation)
-            if self.differentiate.get() == "off":
-                try:
-                    self.result.set(eval(operation))
+            calculation_thread = threading.Thread(
+                target=self.calculate_result, args=(self.formula, self.result)
+            )
+            calculation_thread.start()
+            return
 
-                except SyntaxError:
-                    self.result.set("SYNTAX ERROR")
-
-                except ZeroDivisionError:
-                    self.result.set("MATH ERROR")
-                    self.operation.clear()
-                    self.display_num.clear()
-                return
-            else:
-                self.differentiation_operation(operation)
         elif value == "Del":
             self.display_num.pop()
             self.operation.pop()
@@ -140,7 +181,6 @@ class App(ctk.CTk):
             self.basic_operation(value)
             self.trigonometry_operation(value)
             self.logarithmic_operation(value)
-            print(self.operation)
 
     def display(self, value, operation):
         self.display_num.append(value)
@@ -153,6 +193,7 @@ class App(ctk.CTk):
             "-",
             "x",
             "/",
+            ".",
             "x²",
             "√",
             "E",
@@ -183,6 +224,8 @@ class App(ctk.CTk):
             elif value == "x³":
                 value = f"{value}"
                 self.display("³", "**3")
+            elif value == ".":
+                self.display(value, value)
             elif value == "^":
                 value = f"{value}"
                 self.display("^", "**")
@@ -200,7 +243,10 @@ class App(ctk.CTk):
             elif value == "x!":
                 self.factorial_operation()
             elif value == "SHIFT":
-                self.shift = True
+                if self.shift == True:
+                    self.shift = False
+                else:
+                    self.shift = True
             else:
                 value = f" {value} "
                 self.display(value, value)
@@ -235,6 +281,8 @@ class App(ctk.CTk):
             self.operation.clear()
             derivative_str = printer.doprint(derivative)
             self.display(derivative_str, derivative_str)
+            self.history.append(derivative_str)
+
         except sp.SympifyError:
             self.result.set("SYNTAX ERROR")
 
@@ -279,12 +327,28 @@ class App(ctk.CTk):
 
     def scientific(self, choice):
         if choice == "Scientific":
+            self.diff_widget.place(relx=0.5, rely=0.0, anchor="ne")
             self.frame_normal.grid_forget()
+            self.frame_programmer.place_forget()
+            self.entry.grid(column=0, row=0, sticky="nsew")
             self.frame_scientific.grid(column=0, row=1, sticky="nsew")
 
         if choice == "Normal":
+            self.diff_widget.place_forget()
+            self.frame_programmer.place_forget()
             self.frame_scientific.grid_forget()
+            self.entry.grid(column=0, row=0, sticky="nsew")
             self.frame_normal.grid(column=0, row=1, sticky="nsew")
+
+        if choice == "Programmer":
+            self.diff_widget.place_forget()
+            self.entry.grid_forget()
+            self.frame_scientific.grid_forget()
+            self.frame_normal.grid_forget()
+            self.frame_programmer.place(
+                relx=0.0, rely=0.0, anchor="nw", relwidth=1, relheight=1
+            )
+            Programmer(self)
 
 
 class Entry(ctk.CTkFrame):
@@ -300,10 +364,10 @@ class Entry(ctk.CTkFrame):
         font1 = ctk.CTkFont(family=FONT, size=SMALL, weight="normal")
 
         formula = ctk.CTkLabel(
-            self, text="", font=font1, textvariable=formula, text_color=DEEPBROWN
+            self, text="", font=font1, textvariable=formula, text_color=WHITE
         )
         result = ctk.CTkLabel(
-            self, text="", font=font2, textvariable=result, text_color=DEEPBROWN
+            self, text="", font=font2, textvariable=result, text_color=WHITE
         )
 
         formula.grid(column=0, row=0, sticky="se", padx=10)
@@ -351,7 +415,7 @@ class Button(ctk.CTkButton):
             text=text,
             fg_color=color,
             hover_color=hv_color,
-            text_color=PRIMARY,
+            text_color=WHITE,
             font=font3,
             command=lambda: func(text),
         )
@@ -386,6 +450,31 @@ class ScientificFrame(ctk.CTkFrame):
                 hv_color=data["hv_color"],
                 color=data["color"],
             )
+
+
+class Programmer(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(master=parent, fg_color=PRIMARY)
+        self.configure(corner_radius=0)
+
+        # !---------------WIDGET-------------------
+        ctk.CTkLabel(self, text="Binary", text_color=WHITE).place(relx=0.5, rely=0.1, anchor="center")
+        ctk.CTkEntry(self).place(
+            relx=0.5, rely=0.15, relwidth=0.8, relheight=0.05, anchor="center"
+        )
+        ctk.CTkLabel(self, text="Decimal", text_color=WHITE).place(
+            relx=0.5, rely=0.3, anchor="center"
+        )
+
+        ctk.CTkEntry(self).place(
+            relx=0.5, rely=0.35, anchor="center", relwidth=0.8, relheight=0.05
+        )
+        ctk.CTkLabel(self, text="Hexadecimal", text_color=WHITE).place(
+            relx=0.5, rely=0.5, anchor="center"
+        )
+        ctk.CTkEntry(self).place(
+            relx=0.5, rely=0.55, relwidth=0.8, relheight=0.05, anchor="center"
+        )
 
 
 class SuperscriptPrinter(StrPrinter):
